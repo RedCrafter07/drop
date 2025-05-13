b48a159d751bc82e1a264cd8e48be887e847c6f5
RedCrafter07 <gh@r07.dev>
2025-05-13 11:40:51 +0200

    feat: Initialize sveltekit

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import { svelte, vitePreprocess } from "@sveltejs/vite-plugin-svelte"
-
-export default defineConfig({
-  appType: "spa",
-  plugins: [svelte({ preprocess: vitePreprocess() })],
-  root: "./src/client/"
-})
